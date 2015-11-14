65f49db1bdd22d775772b9c4e6c1ec4c0a375b77
Peter Marton <peter@risingstack.com>
2015-11-14 11:09:47 +0100

    test(protect): create test skeleton for team member check

--- server/utils/protect.e2e.spec.js
+++ server/utils/protect.e2e.spec.js
@@ -5,7 +5,7 @@ const request = require('co-supertest')
 const server = require('../server')
 const userFixtures = require('../fixtures/user')
 
-describe('protect', function () {
+describe('protect middleware', function () {
   let user
 
   beforeEach(function *() {
@@ -18,22 +18,30 @@ describe('protect', function () {
     yield userFixtures.destroyLoggedInUser(user)
   })
 
-  it('should reject invalid token', function * () {
-    const token = 'aaa.aaa.aaa'
+  describe('token access check', () => {
+    it('should reject invalid token', function * () {
+      const token = 'aaa.aaa.aaa'
 
-    yield request(server.listen())
-      .get('/api/user/me')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(401)
-      .expect('Content-Type', /application\/json/)
-      .end()
+      yield request(server.listen())
+        .get('/api/user/me')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(401)
+        .expect('Content-Type', /application\/json/)
+        .end()
+    })
+
+    it('should reject missing token', function * () {
+      yield request(server.listen())
+        .get('/api/user/me')
+        .expect(401)
+        .expect('Content-Type', /application\/json/)
+        .end()
+    })
   })
 
-  it('should reject missing token', function * () {
-    yield request(server.listen())
-      .get('/api/user/me')
-      .expect(401)
-      .expect('Content-Type', /application\/json/)
-      .end()
+  describe('team access', () => {
+    // TODO: move test case from team create
+    it('should accept valid team access')
+    it('should reject invalid team access')
   })
 })
